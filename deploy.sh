6e0f38c1fed9217be48fdc3b43355c98f4095889
Stephen J. Fuhry <steve@montmere.com>
2012-01-30 13:28:25 -0500

    adding vimrc and deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+if [ -f ~/.vimrc ] && [ ! -L ~/.vimrc ]
+then
+    echo "Backing up existing ~/.vimrc to ~/.vimrc.bak"
+    mv ~/.vimrc ~/.vimrc.bak
+fi
+if [ -L ~/.vimrc ]
+then
+    echo "Already have ~/.vimrc symlink, we're good"
+else
+    echo "Creating symlink ~/.vimrc, which points to ~/.vim/vimrc"
+    ln -s ~/.vim/vimrc ~/.vimrc
+fi
---
+++ vimrc
@@ -0,0 +1,4 @@
+call pathogen#infect()
+autocmd vimenter * if !argc() | NERDTree | endif
+let g:NERDTreeDirArrows=0
+
