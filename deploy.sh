946353d506f21dd33bdc6d9e66dd32ee5af2ea43
Stephen J. Fuhry <fuhrysteve@gmail.com>
2014-10-21 20:36:41 -0400

    neovim support!

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,21 @@ then
 fi
 if [ -L ~/.vimrc ]
 then
-    echo "Already have ~/.vimrc symlink, we're good"
+    SYMLINK_LOCATION=$(readlink "$HOME/.vimrc")
+    if [ "$SYMLINK_LOCATION" == "$HOME/.vim/vimrc" ]; then
+        echo "Renaming symlink to point to ~/.vim/nvimrc"
+        rm ~/.vimrc
+        ln -s ~/.vim/nvimrc ~/.vimrc
+    else
+        echo "Already have ~/.vimrc symlink, we're good"
+    fi
 else
     echo "Creating symlink ~/.vimrc, which points to ~/.vim/vimrc"
-    ln -s ~/.vim/vimrc ~/.vimrc
+    ln -s ~/.vim/nvimrc ~/.vimrc
+fi
+if [ ! -d ~/.nvim ] || [ ! -L ~/.nvim ]; then
+    echo "Creating symlink ~/.nvim which points to ~/.vim"
+    ln -s ~/.vim ~/.nvim
 fi
 
 VIMINFO_OWNER=$(stat -c "%U" ~/.viminfo)
